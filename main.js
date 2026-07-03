fb07979c56619103b2d94c9bb2c6af4fc1c90f8b
pedrinho1000grau <pedro.rodrigues.oliveira20@escola.pr.gov.br>
2026-07-03 14:29:47 +0000

    aula 4

---
+++ main.js
@@ -0,0 +1,22 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+       // tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+       // tamanhoSenha = tamanhoSenha+1;
+       tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
